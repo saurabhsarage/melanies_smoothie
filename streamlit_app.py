573bf58449d7762bcfcd63fa885f5f34daf0a3a7
Saurabh Sarage <57210145+saurabhsarage@users.noreply.github.com>
2025-09-06 07:49:53 +0000

    fixed issues

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,8 @@ if ingreadient_list:
     for fruits in ingreadient_list:
         ingreadient_String = ingreadient_String + fruits + ' ' 
         
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruits, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruits,' is ', search_on, '.')
         
         st.subheader(fruits + ' Nutrition  Information')
         smoothiefroot_response = requests.get(f"https://www.smoothiefroot.com/api/fruit/{fruits}")
